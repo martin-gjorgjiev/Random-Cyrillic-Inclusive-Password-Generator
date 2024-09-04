d7dea3e67003bef519651e0260ce199e88d93fa4
martin-gjorgjiev <martin.gorgiev@rocketmail.com>
2024-09-04 12:46:37 +0200

    complete version with unit testing

--- rcipg.py
+++ rcipg.py
@@ -1,28 +1,60 @@
+#import modules
 import string
 import secrets
 
+# define cyrillic char set
+cyrillic=u'љњертѕуиопшѓасдфгхјклчќжзџцвбнмйьюяъщђћз́с́ёыэґіўїє'
+
+# old code from first idea
 def rollAgain(n, list1):
     x=secrets.randbelow(n)
     if x in list1:
         x=rollAgain(n, list1)
     return x
 
+# generate random string based on size and source of characters
 def buildPassword(size, charSet):
-    # for the number of characters in string, for how long is the password
     password = ''
     for i in range (0, size):
         password+=secrets.choice(charSet)
-
     return password
 
+# check if the size input is within specifications
 def checkSize(size):
-    if type(size) != 'int' and size < 0:
-        return 'strSize is not a number or smaller than 0'
+    if type(size) is not int or size < 1:
+        raise ValueError('strSize is not a number or smaller than 0')
+    return True
 
+# check if there is a numeric character in the string
+def checkNumeric(text):
+    for c in text:
+        if c.isnumeric():
+            return True
+    return False
 
+# check if there is a character with uppercase
+def checkUpper(text):
+    if text == text.lower():
+        return False
+    return True
+
+# check if there is a special character in the string
+def checkSpecial(text):
+    for c in text:
+        if c in string.punctuation:
+            return True
+    return False
+
+# check if there is a cyrillic character in the string
+def checkCyrillic(text):
+    for c in text:
+        if c in cyrillic:
+            return True
+    return False
+
+#main function for generating the password
 def generate(strSize, uppercaseChar, specialChar, cyrillicChar):
-    # initialise characters
-    cyrillic=u'љњертѕуиопшѓасдфгхјклчќжзџцвбнмйьюяъщђћз́с́ёыэґіўїє'
+    # initialise the set of characters
     charSet = string.ascii_lowercase
     if type(uppercaseChar) != 'bool' and uppercaseChar:
         charSet += string.ascii_uppercase
@@ -34,8 +66,25 @@ def generate(strSize, uppercaseChar, specialChar, cyrillicChar):
         if type(uppercaseChar) != 'bool' and uppercaseChar:
             charSet += cyrillic.upper()
 
-    # strSize must be number an bigger than zero
+    # strSize must be whole number an bigger than zero
     checkSize(strSize)
 
     # generate password
-    return buildPassword(strSize, charSet) 
+    password = ''
+    # generate passwords until criteria is met
+    while True:
+        check1 = False
+        password = buildPassword(strSize, charSet)
+        # must have at least one numeric value, at least one special character and at least one cyrillic character
+        check1 = checkNumeric(password)
+        if(uppercaseChar):
+            check1 = check1 and checkUpper(password)
+        if(specialChar):
+            check1 = check1 and checkSpecial(password)
+        if(cyrillicChar):
+            check1 = check1 and checkCyrillic(password)
+        # break the loop once criteria is met
+        if(check1):
+            break
+        
+    return password
--- test_rcipg.py
+++ test_rcipg.py
@@ -11,7 +11,47 @@ class TestRcipg (unittest.TestCase):
         self.assertLess(result, 4)
 
     def test_buildPassword (self):
-        pass
+        result = rcipg.buildPassword(8, '12')
+        self.assertIs(len(result), 8)
+        self.assertIn(result[0], '12')
+
+    def test_checkSize (self):
+        self.assertTrue(rcipg.checkSize(8))
+        self.assertRaises(ValueError,rcipg.checkSize,'6')
+        self.assertRaises(ValueError,rcipg.checkSize,-1)
+        self.assertRaises(ValueError,rcipg.checkSize,True)
+
+    def test_checkNumeric(self):
+        self.assertTrue(rcipg.checkNumeric(u'ч*8A'))
+        self.assertFalse(rcipg.checkNumeric(u'ч*aA'))
+
+    def test_checkUpper(self):
+        self.assertTrue(rcipg.checkUpper('aA'))
+        self.assertFalse(rcipg.checkUpper('aa'))
+
+    def test_checkSpecial(self):
+        self.assertTrue(rcipg.checkSpecial(u'ч*8A'))
+        self.assertFalse(rcipg.checkSpecial(u'ч8A'))
+
+    def test_checkCyrillic(self):
+        self.assertTrue(rcipg.checkCyrillic(u'ч*8A'))
+        self.assertFalse(rcipg.checkCyrillic(u'*aA'))
+
+    def test_generate (self):
+        # strSize, uppercaseChar, specialChar, cyrillicChar
+        allResult = rcipg.generate(8,True,True,True)
+        cyrillicResult = rcipg.generate(8,False,False,True)
+        specialResult = rcipg.generate(8,False,True,False)
+        upperResult = rcipg.generate(8,True,False,False)
+
+        self.assertTrue(rcipg.checkUpper(upperResult))
+        self.assertTrue(rcipg.checkSpecial(specialResult))
+        self.assertTrue(rcipg.checkCyrillic(cyrillicResult))
+        self.assertTrue(rcipg.checkNumeric(allResult))
+
+        self.assertFalse(rcipg.checkUpper(specialResult))
+        self.assertFalse(rcipg.checkSpecial(cyrillicResult))
+        self.assertFalse(rcipg.checkCyrillic(upperResult))
 
 if __name__ == '__main__':
     unittest.main()
